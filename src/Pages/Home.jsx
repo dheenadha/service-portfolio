6bea4260d72def07f99c7927cf2bb3da2e255648
dheenadhayalan <dheenadhayalan2004@gmail.com>
2025-04-16 13:01:56 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -19,12 +19,12 @@ const Home = () => {
             <Container className="home-content">
               <Row>
                 <Col md={7} className="home-header">
-                  <h1 style={{ paddingBottom: 15 }} className="heading">
+                  <h2 style={{ paddingBottom: 15 }} className="heading">
                     Hi There!{" "}
                     <span className="wave" role="img" aria-labelledby="wave">
                       👋🏻
                     </span>
-                  </h1>
+                  </h2>
 
                   <h2 className="heading-name">
                     <strong className="main-name">
