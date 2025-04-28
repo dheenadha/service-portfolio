70e54235e51dce03bd630a14b3308b6422aa75ef
dheenadha <dheenadayalan2004@gmail.com>
2025-04-28 18:29:08 +0530

    card

--- src/Components/Investors.jsx
+++ src/Components/Investors.jsx
@@ -12,58 +12,66 @@ const Investors = () => {
       <h1 className="investors-title">Investors</h1>
       <div className="investor-cards-container">
         {/* Card 1 */}
-        <div className="investor-card">
-          <div className="investor-img-container">
-            <img
-              src={investor1}
-              alt="Person 1"
-              className="investor-img"
-            />
-          </div>
-
-          <h2 className="investor-name  ">G.Saranya</h2>
-          <div className="investor-contact fs-5">
-           
-            <p><MdOutlineEmail className="fs-4" />Sagoinvest@gmail.com</p>
-          </div>
-
-          <div className="investor-company fs-4">
-           
-            <p><PiBuildings className="fs-4" />SG Groups</p>
-          </div>
-
-          <div className="investor-location fs-2">
-           
-            <p ><FaLocationDot className="fs-5"/>Vellore</p>
-          </div>
+        
+        <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <div class="profile-image">
+        <div class="name">
+         <h4 className="investor-name p-2  ">SG Group</h4>
         </div>
+        <p className="fs-5 p-2"><FaLocationDot className="fs-4" />Vellore</p>
+      </div>
+    </div>
+    <div class="flip-card-back">
+      <div class="Description">
+        <p class="description">
+        We are a strategically focused company committed to delivering sustainable growth and innovation</p>
+        
+      </div>
+    </div>
+  </div>
+</div>        
+
+          
+       
+       
+           
+            
+       
+       
 
         {/* Card 2 */}
-        <div className="investor-card">
-          <div className="investor-img-container">
-            <img
-              src={investor2}
-              alt="Person 2"
-              className="investor-img"
-            />
-          </div>
-          <h2 className="investor-name">A.Kathiravan</h2>
-
-          <div className="investor-contact fs-5">
-            
-            <p><MdOutlineEmail className="fs-4" />asaikathir@gmail.com</p>
-          </div>
-
-          <div className="investor-company fs-5">
-          
-            <p><PiBuildings className="fs-4" />Sri Durgaiamman Constructions</p>
-          </div>
-
-          <div className="investor-location fs-5">
-          
-            <p><FaLocationDot className="fs-5"/>Kallakurichi</p>
-          </div>
+        <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <div class="profile-image">
+        <div class="name">
+          <h4 className="investor-name">Sri Durgaiamman Constructions</h4>
         </div>
+       <p className="fs-5"><FaLocationDot className="fs-4" />Kallakurichi</p>
+      </div>
+    </div>
+    <div class="flip-card-back">
+      <div class="Description">
+        <p class="description">
+        We are a strategically focused company committed to delivering sustainable growth and innovation         </p>
+        
+      </div>
+    </div>
+  </div>
+</div>
+        
+        
+
+       
+
+          
+            
+        
+
+       
+      
       </div>
     </div>
   );
