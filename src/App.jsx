fa77a80df4194e9ccaa8e650263481474827106d
dheenadha <dheenadayalan2004@gmail.com>
2025-04-28 20:42:46 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,6 @@ import About from "./Pages/About";
 import TeamProfiles from "./Components/TeamProfiles";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Topbar from "./Topbar";
-import ScrollToTop from "./ScrollToTop";
 import React, { useRef, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Nav from "./Components/Nav";
@@ -17,6 +15,7 @@ import TechStack from "./Components/TechStack";
 import ClientCard from "./Components/ClientCard";
 import CEOIntroCard from "./Components/CEOFounder";
 import Investors from "./Components/Investors";
+
 
 function App() {
   const homeRef = useRef(null);
@@ -75,32 +74,6 @@ function App() {
 
   return (
     <>
-      {/* <Nav /> */}
-
-      {/* <BrowserRouter>
-    <Topbar/>
-    <ScrollToTop />
-    <ScrollToTopButton/>
-        <Routes>
-    */}
-      {/* <Route path="/" element={<Topbar/>} > */}
-      {/* <Route index element={<Home/>} />
-    <Route path="/about" element={<About/>} />
-    <Route path="/services" element={<Services/>} />
-    <Route path="/team" element={<TeamProfiles/>} />
-    <Route path="/projects" element={<SampleProjects/>} />
-    <Route path="/contact" element={<Contact/>} />
-    {/* </Route> */}
-      {/* </Routes> */}
-      {/* </BrowserRouter> */}
-      {/* <div id="home"><Home /></div>
-    <div id="about"><About /></div>
-    <div id="services"><Services /></div>
-    <div id="team"><TeamProfiles /></div>
-    <div id="projects"><SampleProjects/></div>
-    <div id="contact"><Contact /></div> */}
-
-      {/* <Footer /> */}
 
       <div>
         <Nav />
